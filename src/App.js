5c840c9521a093ba3e19b87b794029869785e290
Geetha Madhuri <geethagorantla20@gmail.com>
2024-07-25 16:36:32 +0530

    @geethamadhuri javascript Todo item code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [todos,setTodos]=useState([])
+console.log(todos,"todos")
+const initialState={
+  id:"",
+  title:"",
+  completed:false
+}
+const [todoItem,setTodoItem]=useState(initialState)
+
+console.log(todoItem,"todoItem")
+
+const handleTodoItem=(e)=>{
+  const {name,value}=e.target
+  setTodoItem({...todoItem,[name]:value})
+}
+
+const handleADDTodoItem=()=>{
+  if(todoItem?.title !==""){
+ if(todoItem?.id){
+  const findingdata=todos?.map((item)=>{
+    return item?.id === todoItem?.id ? {...item,title:todoItem?.title} : item
+  })
+  setTodos(findingdata)
+ }
+ else{
+  const newTodoItemadding={...todoItem,id:new Date()?.getTime()?.toString(),completed:false}
+  setTodos([...todos,newTodoItemadding])
+  setTodoItem(initialState)
+ }
+}
+else{
+  alert("Please Enter the Title")
+}
+}
+
+const handleEditButton=(id)=>{
+  if(id){
+    const findingtheId=todos?.find((item)=>{
+      return item?.id === id && item
+    })
+    setTodoItem(findingtheId)
+    
+  }
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo__main'>
+      <div>
+        <input className='todo_input' value={todoItem?.title} onChange={handleTodoItem} name="title"/>
+        <button className='todo_button' onClick={handleADDTodoItem}>ADD ITEM</button>
+      </div>
+      <div className='table_todo'>
+        <table>
+        <tr>
+    <th>Title</th>
+    <th>Status</th> 
+    <th>Edit</th>
+    <th>Delete</th>
+  </tr>
+  {
+    todos?.map((item)=>{
+      return <tr>
+       <td>{item?.title}</td>
+       <td>{item?.status ? "completed" : "Inprogress"}</td>
+       <td><button onClick={()=>handleEditButton(item?.id)}>Edit</button></td>
+       <td><button>Delete</button></td>
+      </tr>
+    })
+  }
+ 
+        </table>
+      </div>
+     
     </div>
   );
 }
